70905f7f132c3e8ded060fecf3b757b2f5082340
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-17 14:29:53 -0400

    Rossmoor 2.0 1.1  /contest.vue

--- contest.vue
+++ contest.vue
@@ -120,7 +120,7 @@
                 }); 
                 
                 this.$store.dispatch("getData", "contests").then(response => {
-                    this.currentContest = this.findContestByShowOnSlug('cerritos-shopsatrossmoor-contest');
+                    this.currentContest = this.findContestByShowOnSlug('rossmoor-shopsatrossmoor-contest');
                     this.dataLoaded = true;
                 }, error => {
                     console.error("Could not retrieve data from server. Please check internet connection and try again.");
