ccd6db606f8b416e2281d96b18c58c1f955c7e23
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-17 15:16:16 -0400

    Rossmoor 2.0 1.1  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -60,7 +60,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Leasing Banner').images;
+                    var temp_repo = this.findRepoByName('Leasing Banner');
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
