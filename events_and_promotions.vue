e66408e91aaedc45baca5e70cd80b195a5ccbdb4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-17 15:16:10 -0400

    Rossmoor 2.0 1.1  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -111,7 +111,7 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Events Banner').images;
+                    var temp_repo = this.findRepoByName('Events Banner');
                     if(temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
