d453b54cf58963bc68356bd2ab9cdbf3eb5e914c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-17 15:17:33 -0400

    Rossmoor 2.0 1.1  /contest.vue

--- contest.vue
+++ contest.vue
@@ -107,7 +107,7 @@
             },
             created() {
                 this.$store.dispatch("getData", "repos").then(response => {
-                    var temp_repo = this.findRepoByName('Events Banner').images;
+                    var temp_repo = this.findRepoByName('Events Banner');
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
