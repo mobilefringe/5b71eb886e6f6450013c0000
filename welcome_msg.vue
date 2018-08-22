59fe07d2f30f67cae323e0cf7f32e1ec795ebaf5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-22 13:22:56 -0400

    Rossmoor 2.0 1.1  /welcome_msg.vue

--- welcome_msg.vue
+++ welcome_msg.vue
@@ -17,13 +17,13 @@
                 }
             },
             created(){
-                this.loadData().then(response => {
-                    if(response[0].data.messages.welcome[0] != undefined){
-                        this.currentMessage = response[0].data.messages.welcome[0].messages[0];
-                    } else {
-                        console.log("No message at this time.");       
-                    }
-                });
+                // this.loadData().then(response => {
+                    // if(response[0].data.messages.welcome[0] != undefined){
+                    //     this.currentMessage = response[0].data.messages.welcome[0].messages[0];
+                    // } else {
+                    //     console.log("No message at this time.");       
+                    // }
+                // });
             },
             computed: {
                 ...Vuex.mapGetters([
@@ -31,15 +31,15 @@
                 ])
             },
             methods: {
-                loadData: async function() {
-                    this.property.mm_host = this.property.mm_host.replace("http:", "");
-                    try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/shopsatrossmoor/messages.json"})]);
-                        return results;
-                    } catch(e) {
-                        console.log("Error loading data: " + e.message);    
-                    }
-                }
+                // loadData: async function() {
+                //     this.property.mm_host = this.property.mm_host.replace("http:", "");
+                //     try {
+                //         let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/shopsatrossmoor/messages.json"})]);
+                //         return results;
+                //     } catch(e) {
+                //         console.log("Error loading data: " + e.message);    
+                //     }
+                // }
             }
         })
     });
