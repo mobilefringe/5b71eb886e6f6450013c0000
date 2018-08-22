007925532378e3f232921b239ccb10e0ca649728
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-22 13:51:27 -0400

    Rossmoor 2.0 1.1  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -64,7 +64,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = _.sortBy(this.findRepoByName('Center Information Images'), function(o){});;
+                    var temp_repo = _.sortBy(this.findRepoByName('Center Information Images'), function(o){return o.id});;
                     if(temp_repo) {
                         var three_imgs = _.slice(temp_repo.images, [0], [3])
                         this.pageImages = three_imgs;
