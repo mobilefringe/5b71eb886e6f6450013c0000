3202a20df41d581945536fe73dd9da85ad3077d1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-22 16:50:59 -0400

    Rossmoor 2.0 1.1  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -84,7 +84,7 @@
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Newsletter Banner');
                     if(temp_repo != null && temp_repo != undefined) {
-                        this.pageBanner = temp_repo[0];
+                        this.pageBanner = temp_repo.images[0];
                     } else {
                         this.pageBanner = {
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
