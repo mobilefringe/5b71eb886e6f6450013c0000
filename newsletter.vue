813227b5c020aac4cdee127c6f1acb94846d18b8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-22 16:49:33 -0400

    Rossmoor 2.0 1.1  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -19,10 +19,6 @@
                                         <label for="fieldfihudt" class="accessibility">First Name</label>
                                         <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldfihudt" name="cm-name" type="text" placeholder="First Name">
                                     </div>
-                                    <div class="col-sm-6" >
-                                        <label for="fieldfihudi" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-f-fihudi" type="text" placeholder="Last Name">
-                                    </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
