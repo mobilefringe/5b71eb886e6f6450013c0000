3b244b15b9464734de07f1d25d493393a5a8ed3e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-22 13:23:44 -0400

    Rossmoor 2.0 1.1  /welcome_msg.vue

--- welcome_msg.vue
+++ welcome_msg.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="currentMessage" class="main_container">
         <div class="message_content">
-            <h1 class="center">{{ property.currentMessage.title }}</h1>
+            <h1 class="center">{{ property.welcome_message }}</h1>
             <p class="center">{{ property.description }}</p> 
         </div>
     </div>
