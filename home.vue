78d13c446ba18fa38915cd8e968b9c9d752e5154
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-14 14:23:35 -0400

    Rossmoor 2.0 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -21,7 +21,7 @@
                                 </div>
                             </div>
                             <div v-else-if="!banner.url">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1552587372438/rsb_home_1925x470.jpg)}"></div>
+                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1552587372438/rsb_home_1925x470.jpg)'}"></div>
                             </div>
                             <div v-else>
                                 <a :href="banner.url">
