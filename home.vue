1da447bf75bf6858371c2de9d146e010f89f43e7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-14 14:21:47 -0400

    Rossmoor 2.0 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -6,8 +6,8 @@
                 <div class="home_banner_container">
                     <div class="prev"></div>
                     <slick ref="slick" :options="slickOptions">
-                        <!--<div v-if="homeBanners" v-for="banner in homeBanners">-->
-                            <divclass="banner_height">
+                        <div v-if="homeBanners" v-for="banner in homeBanners">
+                            <div v-if="banner.name && banner.description" class="banner_height">
                                 <div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1552587372438/rsb_home_1925x470.jpg)' }"></div>
                                 <div class="banner_content_container">
                                     <div class="banner_content">
