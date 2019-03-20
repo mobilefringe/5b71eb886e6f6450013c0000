e49b04994aa9e7cce87936ce26a158d1720e8d1e
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-03-20 13:15:00 -0400

    Rossmoor 2.0 1.1  /main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 social_media: [
                     {
                         name: "Facebook",
-                        url: "https://www.facebook.com/ShopCerritos",
+                        url: "https://www.facebook.com/theshopsatrossmoor/",
                         iconClass: "fab fa-facebook"
                     },
                     {
