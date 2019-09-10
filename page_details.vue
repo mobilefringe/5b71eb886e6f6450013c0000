54165bfe36001dd9ca7dff8883bbb22b1aec3177
mustafa@mobilefringe.com @ CodeCloud <mustafa@mobilefringe.com @ CodeCloud>
2019-09-10 16:42:06 -0400

    Rossmoor 2.0 1.2  /page_details.vue

---
+++ page_details.vue
@@ -0,0 +1,92 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                 <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(' + pageBanner.image_url + ') center center rgba(73, 126, 113)' }">
+                    <div class="main_container position_relative">
+                        <h2 v-if="currentPage" v-html="currentPage.title"></h2>
+                    </div>
+                </div>
+                <div class="main_container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <breadcrumb></breadcrumb>
+                        </div>
+                    </div>
+                    <div class="row margin_60">
+                        <div class="col-md-12" v-if="currentPage">
+                            <div class="page_body" v-html="currentPage.body"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function (Vue, Vuex) {
+        return Vue.component("page-details-component", {
+            template: template, // the variable template will be injected,
+            props: ['id'],
+            data: function data() {
+                return {
+                    dataLoaded: false,
+                    currentPage: null
+                }
+            },
+            created() {
+                this.updateCurrentPage(this.id);
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Pages Banner');
+                    if(temp_repo != null && temp_repo != undefined) {
+                        this.pageBanner = temp_repo.images[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5d6ff3ac6e6f647c7f3a0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                        }
+                    }
+                    
+                    this.pageContent = response[0].data;
+                    this.dataLoaded = true;
+                });
+            },
+            watch: {
+                $route: function () {
+                    this.updateCurrentPage(this.$route.params.id);
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'findRepoByName'
+                ])
+            },
+            methods: {
+                updateCurrentPage(id) {
+                    this.property.mm_host = this.property.mm_host.replace("http:", "");
+                    var _this = this;
+                    this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }).then(function (response) {
+                        _this.currentPage = response.data;
+                        _this.$breadcrumbs[0].meta.breadcrumb = _this.currentPage.title
+                        _this.dataLoaded = true;
+                    }, function (error) {
+                        console.error( "Could not retrieve data from server. Please check internet connection and try again.");
+                        _this.$router.replace({ name: '404' });
+                    });
+                    
+                },
+                loadData: async function () {
+                    this.property.mm_host = this.property.mm_host.replace("http:", "");
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
