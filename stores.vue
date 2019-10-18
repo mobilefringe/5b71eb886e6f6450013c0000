f426749bf66400869a95abb6136be1f0fd58b9dd
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-18 16:49:19 -0400

    Rossmoor 2.0 1.2  /stores.vue

--- stores.vue
+++ stores.vue
@@ -208,23 +208,6 @@
                     this.filteredStores = store_list;
                     return store_list
                 },
-                // dropDownCats() {
-                //     var vm = this;
-                //     var store_cats = _.filter(this.processedStores, function(o) { return !_.includes(o.categories, vm.dineFilter)});
-                //     var cats = [];
-                //     _.forEach(store_cats, function(value, key) {
-                //         _.forEach(value.categories, function(category, key) {
-                //             var current_category = vm.findCategoryById(category)
-                //             if(!_.includes(cats, current_category.name)) {
-                                
-                //                 cats.push(current_category.name)
-                //             }
-                //         });
-                //     });
-                //     cats = cats.sort();
-                //     cats.unshift('All');
-                //     return cats;
-                // },
                 dropDownCats() {
                     var vm = this;
                     var cats = _.filter(this.processedCategories, function(o) { return _.toNumber(o.id) !== vm.dineFilter && o.store_ids != null});
